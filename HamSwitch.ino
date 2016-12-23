11f6f3fe6c4f1ba8b4a368d87840b49cb79dc819
Gil (4Z1KD) <gil4z1kd@gmail.com>
2016-12-23 08:19:47 +0200

    Add files via upload

--- HamSwitch.ino
+++ HamSwitch.ino
@@ -1,23 +1,30 @@
 /**************************************************************************
-   Arduino Antenna Switch
-
-   This application allows the user to select between 2 antenna selection modes:
-   1. Automatic + Manual selection (Radio Priority Mode)
-   2. Manual selection (Manual Only Mode)
-
-   In "Radio Priority Mode", the 3to8 decoder selects the correct
-   relay based on the frequency input from the radio, while maintaining the user manual selection capability.
-   if the user turns the rotary encoder, the Antenna Switch will change to "Manual only" mode.
-
-   In "Manual Only Mode", the radio input is ignored (The controller does not send CAT requests) and the 3to8 decoder
-   selects the relay based on the user input from a rotary encoder.
-
-   -- Emergency State --
-   clicking the rotary encoder button changes to "Manual Only Mode" and selects the dummy load.
-
-   License: This code is FREE for private use by Amateur Radio Operators
-   Created: December 2016
-   Design and Code: Gil, 4Z1KD (by request from Dubi, 4Z5DZ)
+  HamSwitch
+  Arduino based auto antenna switch
+  
+  The hardware is based on an Arduino 'Uno' and a 74HC238 - 3 to 8 Line Decoder chip:
+  ![Alt text](https://raw.githubusercontent.com/4Z1KD/HamSwitch/master/74HC238.png?raw=true "74HC238 Chip")
+  
+  The application allows the user to toggle between 2 antenna selection modes:
+  1. Automatic + Manual selection (Radio Priority Mode)
+  2. Manual selection (Manual Only Mode)
+  
+  In "Radio Priority Mode", the 3 to 8 decoder selects the relay (antenna)
+  based on the frequency input from the radio, while maintaining the user manual selection capability.
+  if the user turns the rotary encoder, HamSwitch will change back to "Manual only" mode.
+  
+  In "Manual Only Mode", the radio input is ignored (The controller does not send CAT requests) and the 3 to 8 decoder
+  selects the relay (antenna) based on the user input from the rotary encoder.
+  
+  -- Emergency Button --
+  Clicking the rotary encoder button changes to "Manual Only Mode" and selects the dummy load.
+  
+  Here is the main Schema:
+  ![Alt text](https://raw.githubusercontent.com/4Z1KD/HamSwitch/master/Main%20Schema.PNG?raw=true "HamSwitch Schema")
+  
+  License: This code is FREE for private use by Amateur Radio Operators
+  Created: December 2016
+  Design and Code: Gil, 4Z1KD (by request from Dubi, 4Z5DZ)
 **************************************************************************/
 
 #include <AltSoftSerial.h>

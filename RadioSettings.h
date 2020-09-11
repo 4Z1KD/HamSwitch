2103a0e0e0ed2a1063dc0d8dd8bfd4fa63162c60
Gil (4Z1KD) <gil4z1kd@gmail.com>
2020-09-11 18:26:27 +0300

    Separate pooling implementation foreach CAT command

--- RadioSettings.h
+++ RadioSettings.h
@@ -1,39 +1,139 @@
 #include "Arduino.h"
 #include "UserSettings.h"
 #include <SimpleList.h>
+#include <AltSoftSerial.h>
+
+//Generic Request/Response function definition - every radio should implement these methods
+typedef void (*RadioRequest) (AltSoftSerial* serial);
+typedef unsigned long (*RadioResponse) (AltSoftSerial* serial);
+
+
+//********************************************************** Radio Implementations ******************************************************************//
+//************* 5 byte req/res *************
+void FT8X7_Request(AltSoftSerial* serial)
+{
+  byte ReadFreq[] = {0x00, 0x00, 0x00, 0x00, 0x03};
+  for (byte i = 0; i < sizeof(ReadFreq); i++)
+  {
+    serial->write(ReadFreq[i]);
+  }
+}
+
+unsigned long FT8X7_Response(AltSoftSerial* serial)
+{
+  int i = 0;
+  String response = "";
+  while (serial->available()) {
+    if (i++ < 4)
+    {
+      int inChar = (int)serial->read();
+      response += (inChar < 10) ? "0" + String(inChar, HEX) : String(inChar, HEX);
+      delay(5);
+    }
+    else
+    {
+      serial->read();
+    }
+  }
+  unsigned long j = 0;
+  String pattern = "%lu";
+  sscanf (&response[0], &pattern[0], &j);
+  return j;
+}
+//***************** FA ******************
+void FA_Request(AltSoftSerial* serial)
+{
+  serial->print("FA;");
+}
+
+unsigned long FA_Response(AltSoftSerial* serial)
+{
+  String response = "";
+  while (serial->available()) {
+    char inChar = (char)serial->read();
+    response += inChar;
+    if (inChar == ';') {
+      response.trim(); //trim the string
+      break;
+    }
+    delay(5);
+  }
+  unsigned long j = 0;
+  String pattern = "FA%lu";
+  sscanf (&response[0], &pattern[0], &j);
+  return j/10;
+}
+//**************************************************************************************************************************************************//
 
 //Radio Enumeration
-const int ICOM = 0;
-const int YAESU = 1;
-const int KENWOOD = 2;
-const int ELECRAFT = 3;
+const int FT817 =     101;
+const int FT857 =     102;
+const int FT897 =     103;
+const int FT991 =     104;
+
+const int TS450 =     201;
+const int TS480 =     202;
+const int TS590 =     203;
+const int TS690 =     204;
+const int TS850 =     205;
+const int TS950 =     206;
+
+const int K2 =        301;
+const int KX2 =       302;
+const int K3S =       303;
+const int K3 =        304;
+const int KX3 =       305;
+
+const int SDR1000 =   401;
 
 /*
    This struct represents a radio.
-   It has an Id, some variables that holds supported commands and a matching list of regex patterns to parse the response.
+   It has an Id, and two callbacks for sending commands to the radio and receiving radio response
 */
 struct Radio {
   int Id;
-  String FrequencyCommand;
-  String FrequencyResponsePattern;
+  char* displayname;
+  RadioRequest FrequencyCommand;
+  RadioResponse FrequencyResponse;
 };
 
 //the supported radios at the moment
-Radio DummyRadio = { -1, "", ""};
-Radio Kenwood = {KENWOOD, "FA;", "FA%lu"};
-Radio Icom = {ICOM, "FA;", "FA%lu"};
-Radio Yaesu = {YAESU, "FA;", "FA%lu"};
-Radio Elecraft = {ELECRAFT, "FA;", "FA%lu"};
+Radio DummyRadio =      {-1,        (char*)"DUMMY RADIO", 0,              0             };
+
+Radio FT817_Radio =     {FT817,     (char*)"FT-817",      FT8X7_Request,  FT8X7_Response};
+Radio FT857_Radio =     {FT857,     (char*)"FT-857",      FT8X7_Request,  FT8X7_Response};
+Radio FT897_Radio =     {FT897,     (char*)"FT-897",      FT8X7_Request,  FT8X7_Response};
+Radio FT991_Radio =     {FT991,     (char*)"FT-991",      FA_Request,     FA_Response};
+
+Radio TS450_Radio =     {TS450,     (char*)"TS-450",      FA_Request,     FA_Response};
+Radio TS480_Radio =     {TS480,     (char*)"TS-480",      FA_Request,     FA_Response};
+Radio TS590_Radio =     {TS590,     (char*)"TS-590",      FA_Request,     FA_Response};
+Radio TS690_Radio =     {TS690,     (char*)"TS-690",      FA_Request,     FA_Response};
+Radio TS850_Radio =     {TS850,     (char*)"TS-850",      FA_Request,     FA_Response};
+Radio TS950_Radio =     {TS950,     (char*)"TS-950",      FA_Request,     FA_Response};
+
+Radio K2_Radio =        {K2,        (char*)"K2",          FA_Request,     FA_Response};
+Radio KX2_Radio =       {KX2,       (char*)"KX2",         FA_Request,     FA_Response};
+Radio K3S_Radio =       {K3S,       (char*)"K3S",         FA_Request,     FA_Response};
+Radio K3_Radio =        {K3,        (char*)"K3",          FA_Request,     FA_Response};
+Radio KX3_Radio =       {KX3,       (char*)"KX3",         FA_Request,     FA_Response};
+
+Radio SDR1000_Radio =   {SDR1000,   (char*)"SDR-1000",    FA_Request,     FA_Response};
+
+
+
 
 SimpleList<Radio> RadioList; //a list that holds the supported radios
 
 void InitRadioList()
 {
-  //RadioList.reserve(4);
-  RadioList.push_back(Kenwood);
-  RadioList.push_back(Icom);
-  RadioList.push_back(Yaesu);
-  RadioList.push_back(Elecraft);
+  RadioList.push_back(FT857_Radio);
+  RadioList.push_back(FT897_Radio);
+  RadioList.push_back(FT991_Radio);
+  RadioList.push_back(TS590_Radio);
+  RadioList.push_back(TS480_Radio);
+  RadioList.push_back(K2_Radio);
+  RadioList.push_back(K3_Radio);
 }
 
 Radio GetMyRadio()
@@ -46,15 +146,4 @@ Radio GetMyRadio()
     }
   }
   return DummyRadio;
-}
-
-//********************************************************** Parser **********************************************************************************//
-
-unsigned long ParseFrequencyResponse(String radioResponse)
-{
-  unsigned long i;
-  Radio myRadio = GetMyRadio(); //Get a struct that represents a Radio
-  String pattern = myRadio.FrequencyResponsePattern; //get the FrequencyResponsePattern from the struct
-  sscanf (&radioResponse[0], &pattern[0], &i);
-  return i;
 }
--- UserSettings.h
+++ UserSettings.h
@@ -1,9 +1,24 @@
+//Select the radio number from the list
 /*
-  ICOM 0
-  YAESU 1
-  KENWOOD 2
-  ELECRAFT 3
-*/
-const int MY_RADIO = 2;
-bool IS_DEBUG = false; //debug flag
+FT817     100
+FT857     101
+FT897     102
+FT991     103
 
+TS450     201;
+TS480     202;
+TS590     203;
+TS690     204;
+TS850     205;
+TS950     206;
+
+K2        301
+KX2       301
+K3S       303
+K3        304
+KX3       305
+
+SDR1000   401
+*/
+
+const int MY_RADIO = 101;

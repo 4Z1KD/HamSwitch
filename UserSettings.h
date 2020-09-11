3aadbc481ab7c723e69d283cdd11ca901e70324d
Gil (4Z1KD) <gil4z1kd@gmail.com>
2020-09-11 18:42:55 +0300

    Update legend (Radio list)

--- UserSettings.h
+++ UserSettings.h
@@ -1,19 +1,19 @@
 //Select the radio number from the list
 /*
-FT817     100
-FT857     101
-FT897     102
-FT991     103
+FT817     101
+FT857     102
+FT897     103
+FT991     104
 
-TS450     201;
-TS480     202;
-TS590     203;
-TS690     204;
-TS850     205;
-TS950     206;
+TS450     201
+TS480     202
+TS590     203
+TS690     204
+TS850     205
+TS950     206
 
 K2        301
-KX2       301
+KX2       302
 K3S       303
 K3        304
 KX3       305
@@ -21,4 +21,4 @@ KX3       305
 SDR1000   401
 */
 
-const int MY_RADIO = 101;
+const int MY_RADIO = 104;
